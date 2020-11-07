eaa0ba61fc490df435cb73bb2c8d215366788275
Patryk Jastrzębski <p.jastrzebski@skilio.pl>
2020-11-07 23:53:03 +0100

    Add mobile haker logo

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,19 +1,33 @@
 $sidebar = $("#sidebar");
 $sidebarCollapse = $("#sidebarCollapse");
+$mobileHeader = $("#mobile-header");
 windowWidth = $(window).width();
 
 $(function() {
     $($sidebarCollapse).on("click", function() {
-        if ($($sidebar).toggleClass("active").hasClass("active")) {
+        if (
+            $($sidebar)
+                .toggleClass("active")
+                .hasClass("active")
+        ) {
             $($sidebarCollapse).addClass("rotate180");
+            $($mobileHeader).fadeOut(250);
         } else {
+            $($mobileHeader).fadeIn(250);
             $($sidebarCollapse).removeClass("rotate180");
         }
     });
 
-    if (windowWidth <= 768) {
-        $($sidebar).removeClass("active");
-    }
-
     AOS.init();
+
+    $(window).on("resize", function() {
+        if ($(this).width() >= 768) {
+            $($mobileHeader).hide();
+            $($sidebar).addClass("active");
+        } else {
+            $($mobileHeader).show();
+            $($sidebar).removeClass("active");
+            $($sidebarCollapse).removeClass("rotate180");
+        }
+    });
 });
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -27,6 +27,11 @@
 <body>
     <div class="wrapper" data-aos="fade-in" data-aos-delay="50" data-aos-duration="500" data-aos-easing="ease-in-out"
         data-aos-once="true">
+
+
+        <div id="mobile-header" data-aos="fade-in" data-aos-delay="150" data-aos-duration="600"
+            data-aos-easing="ease-in-out" data-aos-once="true">&lt;&sol;&gt;haker.edu.pl</div>
+
         <!-- Sidebar  -->
         <nav id="sidebar" class="active" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1500"
             data-aos-easing="ease-in-out" data-aos-once="true">
@@ -358,9 +363,8 @@
     </footer>
 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
