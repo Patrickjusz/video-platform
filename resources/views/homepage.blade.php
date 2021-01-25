c851683ad4c5792c0e83fff13b1ae752c5a23ef5
Patryk <patryk.jastrzebski91@gmail.com>
2021-01-25 19:57:51 +0100

    Load video from database

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,199 +1,32 @@
 @section('page_title', 'Wideo Haker')
 
-@extends('layouts.app')
-@extends('layouts.head')
-@extends('layouts.search')
-@extends('layouts.sidebar')
-@extends('layouts.footer')
+    @extends('layouts.app')
+    @extends('layouts.head')
+    @extends('layouts.search')
+    @extends('layouts.sidebar')
+    @extends('layouts.footer')
 
 @section('content')
-<div class="row video-wrapper" data-aos="zoom-out-up" data-aos-delay="50" data-aos-duration="500"
-    data-aos-easing="ease-in-out" data-aos-once="true">
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://pbs.twimg.com/media/CcdYVn0W0AAPeIs.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
+    <div class="row video-wrapper" data-aos="zoom-out-up" data-aos-delay="50" data-aos-duration="500"
+        data-aos-easing="ease-in-out" data-aos-once="true">
 
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img"
-            src="https://gfx.wiadomosci.radiozet.pl/var/radiozetwiadomosci/storage/images/swiat/wyciekly-dane-235-mln-uzytkownikow-instagrama-tiktoka-i-youtube-a/8128549-1-pol-PL/Wyciekly-dane-235-mln-uzytkownikow-Instagrama-TikToka-i-YouTube-a_article.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">5 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSolIL7K4RQV506VVWoavHHD9CBeEVXvA1KZA&usqp=CAU" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/Z0ojy9wo52w/mqdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/E43datlHvKs/maxresdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/QBRRL-Tz6eE/maxresdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/pWlNhYn5d60/hqdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://pbs.twimg.com/media/CcdYVn0W0AAPeIs.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img"
-            src="https://gfx.wiadomosci.radiozet.pl/var/radiozetwiadomosci/storage/images/swiat/wyciekly-dane-235-mln-uzytkownikow-instagrama-tiktoka-i-youtube-a/8128549-1-pol-PL/Wyciekly-dane-235-mln-uzytkownikow-Instagrama-TikToka-i-YouTube-a_article.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">5 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSolIL7K4RQV506VVWoavHHD9CBeEVXvA1KZA&usqp=CAU" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/Z0ojy9wo52w/mqdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/E43datlHvKs/maxresdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/QBRRL-Tz6eE/maxresdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-
-    <a href="#" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
-        <img class="img" src="https://i.ytimg.com/vi/pWlNhYn5d60/hqdefault.jpg" />
-        <div class="description">
-            <div class="title">Jak zostać hakerem?</div>
-            <div class="stats">
-                <div class="row">
-                    <div class="col-7 views">2,3tys wyświetleń</div>
-                    <div class="col-5 date">3 dni temu</div>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
+        @forelse ($videos as $video)
+            @if (isset($video->slug))
+                <a href="{{ $video->slug }}" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box">
+                    <img class="img" src="https://pbs.twimg.com/media/CcdYVn0W0AAPeIs.jpg" />
+                    <div class="description">
+                        <div class="title" data-full-title="{{ $video->name }}">{{ Str::limit($video->name, 23) }}</div>
+                        <div class="stats">
+                            <div class="row">
+                                <div class="col-7 views">2,3tys wyświetleń</div>
+                                <div class="col-5 date">3 dni temu</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            @endif
+        @empty
+            Obecnie w serwisie nie ma żadnego wideo.
+        @endforelse
+    </div>
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+// Route::get('/', function () {
+//     return view('homepage');
+// });
 
 
 Route::get('/wideo', function () {
     return view('video');
 });
+
+
+Route::get('/', [HomepageController::class, 'index']);
