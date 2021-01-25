5f6f423458c74fa42d6901e0af55ee29b0d4ce98
Patryk <patryk.jastrzebski91@gmail.com>
2021-01-25 19:43:29 +0100

    Crete video seeder

---
+++ database/seeders/VideoSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+
+// php artisan db:seed --class=VideoSeeder
+class VideoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['#haker', '#xss', '#sql', '#css', '#lfi', '#linux'];
+        foreach (range(0, 30) as $i) {
+            DB::table('videos')->insert([
+                'created_at' => Carbon::now(),
+                'name' => Str::random(100),
+                'filename' => Str::random(16) . '.mp4',
+                'imagename' => Str::random(16) . '.jpg',
+                'tags' => $tags[array_rand($tags , 1)].$tags[array_rand($tags , 1)],
+                'slug' => (Str::random(3) . '-' . Str::random(7) . '-' . Str::random(9)),
+                'views' => random_int(0, 99999),
+                'state' => 'active',
+            ]);
+        }
+    }
+}
