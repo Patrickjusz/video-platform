58e13235846cd3fd2c9f91bada63c10fb6a2b47e
Patryk <patryk.jastrzebski91@gmail.com>
2021-01-25 19:19:13 +0100

    Add videos table (migration file)

---
+++ database/migrations/2021_01_25_181210_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('filename');
+            $table->string('thumb');
+            $table->string('tags');
+            $table->integer('views');
+            $table->string('slug');
+            $table->enum('state', ['active', 'inactive', 'delete']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
