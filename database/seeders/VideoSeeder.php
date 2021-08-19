8c5bc265a15720917153560571c59f1cd182ceb0
Patryk S <patryk.jastrzebski91@gmail.com>
2021-08-19 11:37:59 +0200

    Add description column to video model

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -21,16 +21,19 @@ class VideoSeeder extends Seeder
     {
         $tags = ['#haker', '#xss', '#sql', '#css', '#lfi', '#linux'];
         $state = ['active', 'active', 'inactive', 'delete'];
+        $description = simplexml_load_file('http://www.lipsum.com/feed/xml?amount=1&what=paras&start=' . RAND(1, 100))->lipsum;
+
         foreach (range(0, 30) as $i) {
             DB::table('videos')->insert([
                 'created_at' => Carbon::now(),
                 'name' => Str::random(100),
                 'filename' => Str::random(16) . '.mp4',
-                'imagename' => Str::random(16) . '.jpg',
+                'thumb' => Str::random(16) . '.jpg',
                 'tags' => $tags[array_rand($tags, 1)] . $tags[array_rand($tags, 1)],
                 'slug' => (Str::random(3) . '-' . Str::random(7) . '-' . Str::random(9)),
                 'views' => random_int(0, 99999),
                 'state' => $state[array_rand($state, 1)],
+                'description' => $description
             ]);
         }
     }
