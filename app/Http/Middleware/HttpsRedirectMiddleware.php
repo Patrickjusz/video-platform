60f88f6c990558c65a9750bfc1acd4d60f74bb7b
Patryk S <patryk.jastrzebski91@gmail.com>
2021-10-04 22:17:36 +0200

    Add https redirect middleware

---
+++ app/Http/Middleware/HttpsRedirectMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HttpsRedirectMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->secure() && !isDev()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
