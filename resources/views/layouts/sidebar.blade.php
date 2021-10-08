f8930f83bf242bfce5eaa6971ca6c5083bcd2407
Patryk S <patryk.jastrzebski91@gmail.com>
2021-10-08 14:22:00 +0200

    Add h1 tag to homepage

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,9 +2,17 @@
     <nav id="sidebar" class="active" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1500"
         data-aos-easing="ease-in-out" data-aos-once="true">
         <a href="{{ route('video.index') }}" class="sidebar-header">
-            <h3>&lt;&sol;&gt;haker.edu.pl</h3>
+            <h3>&lt;&sol;&gt; Haker Wideo</h3>
             <strong>&lt;&sol;&gt;</strong>
         </a>
+        <div>
+            @if (Route::current()->getName() == 'video.popular' || Route::current()->getName() == 'video.index')
+                <h1 class="header-text">Haker Wideo</h1>
+            @else
+                <p class="header-text">Haker Wideo</p>
+            @endif
+        </div>
+
 
         <ul class="list-unstyled components">
             @foreach ($menu as $element)
