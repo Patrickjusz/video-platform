d525ddab340414b232487acf96941a6c2025689e
Patryk S <patryk.jastrzebski91@gmail.com>
2021-10-08 16:40:53 +0200

    Del h3

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,6 @@
     <nav id="sidebar" class="active" data-aos="fade-right" data-aos-delay="150" data-aos-duration="1500"
         data-aos-easing="ease-in-out" data-aos-once="true">
         <a href="{{ route('video.index') }}" class="sidebar-header">
-            <h3>&lt;&sol;&gt; Haker Wideo</h3>
             <strong>&lt;&sol;&gt;</strong>
         </a>
         <div>
