6902a775980f4836d01763a276be83be4ec7c6b2
Patryk S <patryk.jastrzebski91@gmail.com>
2021-10-10 02:01:41 +0200

    Add SEO title/tags

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,10 +1,11 @@
-@section('page_title', 'Wideo Haker')
-@section('page_description', 'opis')
-@section('page_keywords', 'haker')
+@section('page_title', 'Haker Film')
+@section('page_description', 'Haker film to serwis oferujący darmowe filmy o hakerach i narzędziach takich jak Kali
+    Linux. Znajdziesz tutaj filmy o hakerach i ich programy hakerskie.')
+@section('page_keywords', 'haker film, hakerzy film, filmy haker, filmy o hakerach, wideo haker, kali linux, haker edu
+    wideo')
 @section('og_image', '')
-@section('article_published_time', $latest_video->created_at)
+@section('article_published_time', $latest_video->created_at ?? '')
 @section('article_modified_time', $latest_video->updated_at ?? $latest_video->created_at)
-
 
 @extends('layouts.app')
 @extends('layouts.head')
@@ -21,13 +22,29 @@
                 <h2>Kategoria: <span class="badge badge-secondary">{{ $title }}</span></h2>
             </div>
         @endif
-
+        {{ $specialAlt = ' ' }}
         @forelse ($videos as $video)
             @if (isset($video->slug))
+                <?php
+                switch ($loop->index) {
+                    case 0:
+                        $specialAlt = 'Haker film';
+                        break;
+                    case 1:
+                        $specialAlt = 'Hakerzy film';
+                        break;
+                    case 2:
+                        $specialAlt = 'Filmy haker edu';
+                        break;
+                    default:
+                        $specialAlt = '';
+                }
+                ?>
+
                 <a href="{{ url($video->slug) }}" class="col-xl-3 col-lg-4 col-md-6 col-sm-12 video-box"
                     title="{{ $video->name }}">
-                    <img class="img" src="{{ url(Storage::url($video->thumb)) }}" alt="{{ $video->name }}"
-                        title="{{ $video->name }}" />
+                    <img class="img" src="{{ url(Storage::url($video->thumb)) }}"
+                        alt="{{ $specialAlt }} - {{ $video->name }}" title="{{ $video->name }}" />
                     <div class="duration">{{ empty($video->duration) ? $video->getDuration() : $video->duration }}
                     </div>
                     <div class="description">
