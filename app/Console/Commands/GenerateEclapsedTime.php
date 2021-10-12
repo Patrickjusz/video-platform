d680948804aef05a5b83e41280826899567c1ab5
Patryk S <patryk.jastrzebski91@gmail.com>
2021-10-12 21:18:27 +0200

    Change class name in GenerateEclapsedTime file

--- app/Console/Commands/GenerateEclapsedTime.php
+++ app/Console/Commands/GenerateEclapsedTime.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
-class GenerateSitemap extends Command
+class GenerateEclapsedTime extends Command
 {
     /**
      * The name and signature of the console command.
