a3310367570439de547595711682cdd0e47b02c3
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-02 18:31:43 +0100

    Add translation file (PL)
    Comments feature

---
+++ resources/lang/vendor/comments/pl/comments.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'there_are_no_comments' => 'Film nie posiada jeszcze żadnych komentarzy',
+    'authentication_required' => 'Wymagane uwierzytelnienie',
+    'you_must_login_to_post_a_comment' => 'Musisz się zalogować aby dodać komentarz.',
+    'log_in' => 'Zaloguj się',
+    'reply' => 'Odpowiedz',
+    'edit' => 'Edytuj',
+    'delete' => 'Usuń',
+    'edit_comment' => 'Edytuj komentarz',
+    'update_your_message_here' => 'Zaktualizuj swoją wiadomość tutaj:',
+    'update' => 'Aktualizuj',
+    'cancel' => 'Anuluj',
+    'reply_to_comment' => 'Odpowiedz na komentarz',
+    'enter_your_message_here' => 'Wpisz swoją wiadomość tutaj:',
+    // 'markdown_cheatsheet' => '<a target="_blank" href=":url">Markdown</a> cheatsheet.',
+    'markdown_cheatsheet' => '',
+    'submit' => 'Dodaj',
+    'your_message_is_required' => 'Wiadomość jest wymagana.',
+    'enter_your_message_here' => 'Twój komentarz:',
+    'enter_your_email_here' => 'Wpisz swój adres e-mail:',
+    'enter_your_name_here' => 'Wpisz swoje imię:',
+];
