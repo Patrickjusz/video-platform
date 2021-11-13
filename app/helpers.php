2329241af2faf083e3615b699df4df2792a9fbe5
Patryk <patryk.jastrzebski91@gmail.com>
2021-11-13 20:56:59 +0100

    Add slugify function in helpers file

--- app/helpers.php
+++ app/helpers.php
@@ -201,3 +201,39 @@ if (!function_exists('getApiKey')) {
         return env("API_KEY");
     }
 }
+
+if (!function_exists('slugify')) {
+    /**
+     * slugify
+     *
+     * @param  string $text
+     * @param  string $divider
+     * @return string
+     */
+    function slugify(string $text, string $divider = '-'): string
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+}
